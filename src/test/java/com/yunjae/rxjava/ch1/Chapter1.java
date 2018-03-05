577b1e974d54bf7341eb571c9f158206933ee7cb
comp1 <chotom73@gmail.com>
2018-03-06 06:51:25 +0900

    2018.03.06 commit

--- src/test/java/com/yunjae/rxjava/ch1/Chapter1.java
+++ src/test/java/com/yunjae/rxjava/ch1/Chapter1.java
@@ -4,11 +4,17 @@ import com.yunjae.rxjava.util.Sleeper;
 import org.junit.Ignore;
 import org.junit.Test;
 import rx.Observable;
+import rx.Scheduler;
+import rx.Single;
+import rx.schedulers.Schedulers;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 @Ignore
@@ -117,6 +123,9 @@ public class Chapter1 {
         c.subscribe(System.out::println);
     }
 
+    /**
+     * lazy evaluation
+     */
     @Test
     public void sample_14() {
         String args = SOME_KEY;
@@ -128,10 +137,120 @@ public class Chapter1 {
             });
         });
         someData.subscribe(System.out::println);
+
+        someData.subscribe(s -> System.out.println("Subscriber1 :" + s));
+        someData.subscribe(s -> System.out.println("Subscriber2 :" + s));
+
+        Observable<String> lazyFallback = Observable.just("Fallback");
+        someData.onErrorResumeNext(lazyFallback)
+                .subscribe(System.out::println);
+    }
+
+    /**
+     * duality
+     */
+    @Test
+    public void sample_16() {
+        // Iterable
+        getDataFromLocalMemorySynchronously()
+                .skip(10)
+                .limit(5)
+                .map(s -> s + "_transformed")
+                .forEach(System.out::println);
+
+        System.out.println("########################");
+
+        // Observable
+        getDataFromNetworkAsynchronously()
+                .skip(10)
+                .limit(5)
+                .map(s -> s + "_transformed")
+                .subscribe(System.out::println);
+    }
+
+    private Stream<String> getDataFromLocalMemorySynchronously() {
+        return IntStream
+                .range(0, 100)
+                .mapToObj(Integer::toString);
+    }
+
+    private Observable<String> getDataFromNetworkAsynchronously() {
+        return Observable
+                .range(0, 100)
+                .map(Object::toString);
+    }
+
+    /**
+     * Composition Future(CompletableFuture)
+     */
+    @Test
+    public void sample_19() {
+        CompletableFuture<String> f1 = getDataAsFuture(1);
+        CompletableFuture<String> f2 = getDataAsFuture(2);
+
+        CompletableFuture<String> f3 = f1.thenCombine(f2, (x, y) -> {
+            System.out.println("x + y = " + (x+y));
+            return x + y;
+        });
+    }
+
+    /**
+     * Composition Observable
+     */
+    @Test
+    public void sample_19_2() {
+        Observable<String> o1 = getDataAsObservable(1);
+        Observable<String> o2 = getDataAsObservable(2);
+
+        Observable<String> o3 = Observable.zip(o1, o2, (x, y) -> {
+            return x + y;
+        });
+        o3.subscribe(System.out::println);
+
+        System.out.println("=============================");
+
+        Observable<String> o4 = Observable.merge(o1, o2);
+        o4.subscribe(System.out::println);
+    }
+
+    @Test
+    public void sample_20() {
+        Observable<String> a_merage_b = getDataA().mergeWith(getDataB());
+        a_merage_b.subscribe(System.out::println);
+    }
+
+    public Single<String> getDataA() {
+        return Single.<String>create(o -> {
+            o.onSuccess("DataA");
+        }).subscribeOn(Schedulers.io());
+    }
+
+    private Single<String> getDataB() {
+        return Single.just("DataB").subscribeOn(Schedulers.io());
+    }
+
+    @Test
+    public void sample_21() {
+        Single<String> s1 = getDataAsSingle(1);
+        Single<String> s2 = getDataAsSingle(2);
+
+        Observable<String> s3 = Single.merge(s1, s2);
+        s3.subscribe(System.out::println);
+    }
+
+    private Single<String> getDataAsSingle(int i) {
+        return Single.just("Done: " + i);
     }
 
 
 
+    private CompletableFuture<String> getDataAsFuture(int num) {
+        return CompletableFuture.completedFuture("Done " + num + "\n");
+    }
+
+    private Observable<String> getDataAsObservable(int num) {
+        return Observable.just("Done " + num + "\n");
+    }
 
 
     private Callback getDataAsynchronously(String someKey) {
